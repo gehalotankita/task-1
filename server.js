c512ba98e9cd0361e3941ff4e3a4fa2ae8e927ab
Ankita Gehalot <gehalotankita@gmail.com>
2017-12-09 20:42:39 +0530

    #3 set cookie

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get("/", function(req, res) {
     res.send("Hello World Ankita!");
 });
 
-app.listen(8080,function() {
+app.get("/setcookie", function(req, res){
+    res.cookie('name', 'ankita');
+    res.cookie('age',25);
+    res.end();
+});
+
+
+app.listen(8080,function(){
     console.log("starting server");
 });
+
