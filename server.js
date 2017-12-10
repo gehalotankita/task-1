37d9502dc0fc3af229f51a70d6b344f67a092697
Ankita Gehalot <gehalotankita@gmail.com>
2017-12-10 11:28:34 +0530

    Tasks #5, #6, #7

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 console.log("starting server");
 
 var app =  express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
     res.send("Hello World Ankita!");
@@ -18,7 +22,53 @@ app.get("/getcookie", function(req,res){
      res.send(req.headers.cookie);
 });
 
+app.get("/robots.txt", function(req, res){
+    res.status(403).end();
+})
+
+app.get("/html", function(req,res){
+    res.send(
+        `
+        <html>
+            <body>
+                <div>
+                    <h1>First Page</h1>
+                    <p>This is my first paragraph in html</p>
+                </div>
+            </body>
+        </html>
+        `
+        )
+});
+
+app.get("/input",function(req,res){
+    res.send(
+       ` 
+        <html>
+        <body>
+            <h1>Form Submission</h1>
+            <form  method="post">
+                <input name="UserName" type="text"/>
+                <input type="submit" value="OK"/>
+            </form>
+        </body>
+        </html>
+       `);
+});
+
+app.post("/input",function(req,res){
+    res.send(
+       ` 
+        <html>
+        <body>
+            <h1>Recieved</h1>
+        </body>
+        </html>
+       `);
+    console.log(req.body.UserName);
+});
+
 app.listen(8080,function(){
-    console.log("starting server");
+    console.log("started server");
 });
 
